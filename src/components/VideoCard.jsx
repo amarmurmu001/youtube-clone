062dfaca6ae1210e5cedcddab829162e65100a28
Amar Murmu <iron57441@gmail.com>
2023-11-07 15:28:20 +0530

    feat: navigation

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./VideoCard.css";
+import { useNavigate } from "react-router-dom";
 
 function VideoCard() {
+  const navigate = useNavigate()
+
+  const handleClick = () =>{
+    navigate("/videos")
+  }
+
+
   return (
     <>
-      <div className="video-container">
+      <div className="video-container" onClick={handleClick}>
         <img src="https://i.ytimg.com/vi/Pm-wNmS9RGI/hq720.jpg" alt="" srcset="" />
         <div className="text-container">
           <div className="icon">
